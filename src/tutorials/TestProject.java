107d03725e836379e25101de447e41c6aae03144
whatif85 <geeckoguy@gmail.com>
2016-01-22 14:14:47 -0500

    Added test file

---
+++ src/tutorials/TestProject.java
@@ -0,0 +1,9 @@
+package testproject;
+
+public class TestProject {
+
+       public static void main(String[] args) {
+        System.out.println("Hello, World! This is for you, Github.");
+    }
+    
+}
